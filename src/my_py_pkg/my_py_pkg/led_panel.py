cc644fa454047676020edad26b0a2af4335deff9
Arin Kumar <arinkumar10@outlook.com>
2026-04-06 15:08:44 +0000

    Created custom service interface to set led power on/off

--- src/my_py_pkg/my_py_pkg/led_panel.py
+++ src/my_py_pkg/my_py_pkg/led_panel.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 
 from custom_interfaces.msg import LedState
+from custom_interfaces.srv import SetLed
 
 
 class LedPanel(Node):
@@ -13,6 +14,8 @@ class LedPanel(Node):
             LedState, "led_panel_state", 10
         )
 
+        self.led_setter_service_ = self.create_service(SetLed, "set_led", self.setLed)
+
         self.get_logger().info("Led Panel has started")
         self.create_timer(1, self.publishLEDState)
 
@@ -22,6 +25,8 @@ class LedPanel(Node):
 
         self.led_state_publisher_.publish(led_state_msg_)
 
+    def setLed(self, request, response):
+        pass
 
 def main(args=None):
     rclpy.init(args=args)
