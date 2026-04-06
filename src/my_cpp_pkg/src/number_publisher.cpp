671def3993a010de4d4f6a45017094a9c705fdfc
Arin Kumar <arinkumar10@outlook.com>
2026-04-06 22:44:53 +0000

    Added parameters to number publisher node

--- src/my_cpp_pkg/src/number_publisher.cpp
+++ src/my_cpp_pkg/src/number_publisher.cpp
@@ -1,23 +1,33 @@
 #include "example_interfaces/msg/int64.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include <chrono>
 
 class NumberPublisher : public rclcpp::Node {
 public:
   NumberPublisher() : rclcpp::Node("number_publisher") {
     RCLCPP_INFO(this->get_logger(), "Number Publisher CPP Node has Strated!");
+
+    this->declare_parameter("number", 2);
+    this->declare_parameter("freq", 0.5);
+
+    number_ = this->get_parameter("number").as_int();
+    freq_ = this->get_parameter("freq").as_double();
+
     this->num_publisher_ =
         this->create_publisher<example_interfaces::msg::Int64>("number", 10);
-    this->timer_ = this->create_wall_timer(std::chrono::seconds(1),
+    this->timer_ = this->create_wall_timer(std::chrono::duration<double>(freq_),
                                            [this]() { this->publishNumber(); });
   }
 
 private:
   rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr num_publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+  int number_;
+  double freq_;
 
   void publishNumber() {
     auto msg = example_interfaces::msg::Int64();
-    msg.data = 69;
+    msg.data = number_;
     this->num_publisher_->publish(msg);
     RCLCPP_INFO(this->get_logger(), "Number Published");
   }
