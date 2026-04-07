876eba0b0d62d4da132abb0387dce1c3a2a6c5b2
Arin Kumar <arinkumar10@outlook.com>
2026-04-07 16:21:47 +0000

    Added parameters yaml file to load parameters and added freq and number parameters in number publisher node

--- src/my_cpp_pkg/src/number_publisher.cpp
+++ src/my_cpp_pkg/src/number_publisher.cpp
@@ -1,6 +1,9 @@
 #include "example_interfaces/msg/int64.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include <chrono>
+#include <rclcpp/parameter.hpp>
+#include <rclcpp/parameter_event_handler.hpp>
+#include <vector>
 
 class NumberPublisher : public rclcpp::Node {
 public:
@@ -9,6 +12,12 @@ public:
 
     this->declare_parameter("number", 2);
     this->declare_parameter("freq", 0.5);
+
+    param_callback_handle = this->add_post_set_parameters_callback(
+        [this](const std::vector<rclcpp::Parameter> &params)
+            -> rcl_interfaces::msg::SetParametersResult {
+          return this->handleParamCallback(params);
+        });
 
     number_ = this->get_parameter("number").as_int();
     freq_ = this->get_parameter("freq").as_double();
@@ -24,6 +33,23 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;
   int number_;
   double freq_;
+  PostSetParametersCallbackHandle::SharedPtr param_callback_handle;
+
+  rcl_interfaces::msg::SetParametersResult
+  handleParamCallback(const std::vector<rclcpp::Parameter> &params) {
+    rcl_interfaces::msg::SetParametersResult result;
+    result.successful = true;
+    result.reason = "success";
+
+    for (auto it : params) {
+      if (it.get_name() == "number") {
+        number_ = it.as_int();
+      } else if (it.get_name() == "freq") {
+        freq_ = it.as_double();
+      }
+    }
+    return result;
+  }
 
   void publishNumber() {
     auto msg = example_interfaces::msg::Int64();
