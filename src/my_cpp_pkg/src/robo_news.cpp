4c528ec3e0aa71acd9624b74a93a36a1bff478c8
Arin Kumar <arinkumar10@outlook.com>
2026-04-07 17:27:42 +0000

    Added parameters for robot names to robo news node and added a launch file to launch 5 radio stations with different names and messages using param yaml file.

--- src/my_cpp_pkg/src/robo_news.cpp
+++ src/my_cpp_pkg/src/robo_news.cpp
@@ -1,22 +1,54 @@
 #include "example_interfaces/msg/string.hpp"
+#include "rcl_interfaces/msg/set_parameters_result.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include <rclcpp/parameter.hpp>
+#include <string>
+#include <vector>
 
 class RoboNews : public rclcpp::Node {
 public:
-  RoboNews() : Node("robot_news") {
+  RoboNews() : Node("robot_news"), name{"C-BOT"} {
     RCLCPP_INFO(get_logger(), "Robo News Started (CPP)");
     publisher_ = this->create_publisher<example_interfaces::msg::String>(
         "robot_news", 10);
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&RoboNews::publisherCallback, this));
+
+    this->declare_parameter("name", name);
+
+    param_callback_handler = this->add_post_set_parameters_callback(
+        [this](const std::vector<rclcpp::Parameter> &params)
+            -> rcl_interfaces::msg::SetParametersResult {
+          return this->handleParamCallback(params);
+        });
+
+    timer_ =
+        this->create_wall_timer(std::chrono::milliseconds(500),
+                                std::bind(&RoboNews::publisherCallback, this));
   }
 
 private:
   rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+  PostSetParametersCallbackHandle::SharedPtr param_callback_handler;
+  std::string name;
+
+  rcl_interfaces::msg::SetParametersResult
+  handleParamCallback(const std::vector<rclcpp::Parameter> &param) {
+    rcl_interfaces::msg::SetParametersResult result;
+    result.successful = true;
+    result.reason = "Success";
+
+    for (auto it : param) {
+      if (it.get_name() == "name") {
+        name = it.as_string();
+      }
+    }
+
+    return result;
+  }
 
   void publisherCallback() {
     auto msg = example_interfaces::msg::String();
-    msg.data = std::string("HELLO WORLD");
+    msg.data = std::string("HELLO from " + name);
     // msg.set__data("HELLOW WORLD");
     this->publisher_->publish(msg);
   }
