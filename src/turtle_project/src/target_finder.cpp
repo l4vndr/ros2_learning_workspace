4b9b5a3260707fff03c980edf0b9b771a1179ca9
Arin Kumar <arinkumar10@outlook.com>
2026-04-08 21:55:11 +0000

    Added logic to retrive the latest turtle coords

--- src/turtle_project/src/target_finder.cpp
+++ src/turtle_project/src/target_finder.cpp
@@ -1,7 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtle_project_interfaces/msg/spawned_turtle.hpp"
 #include "turtle_project_interfaces/srv/turtle_coords.hpp"
+#include <chrono>
 #include <memory>
+#include <rclcpp/client.hpp>
+#include <vector>
 
 class TargetFinder : public rclcpp::Node {
 public:
@@ -16,11 +19,34 @@ public:
   }
 
 private:
-  void handleNewTurtleSpawn() {}
+  void handleNewTurtleSpawn() {
+    auto request = std::make_shared<
+        turtle_project_interfaces::srv::TurtleCoords::Request>();
+
+    while (!turtle_coords_client_->wait_for_service(std::chrono::seconds(1))) {
+      RCLCPP_WARN(get_logger(), "Waiting for spawn server");
+    }
+
+    auto future = turtle_coords_client_->async_send_request(
+        request,
+        [this](rclcpp::Client<turtle_project_interfaces::srv::TurtleCoords>::
+                   SharedFuture future) { handleNewTurtleCoords(future); });
+  }
+
+  void handleNewTurtleCoords(
+      rclcpp::Client<turtle_project_interfaces::srv::TurtleCoords>::SharedFuture
+          future) {
+    auto result = future.get();
+    std::vector<double> coords{result->x, result->y, result->yaw_in_rad};
+
+    this->coords_.push_back(coords);
+  }
   rclcpp::Subscription<turtle_project_interfaces::msg::SpawnedTurtle>::SharedPtr
       turtle_spawned_subscription_;
   rclcpp::Client<turtle_project_interfaces::srv::TurtleCoords>::SharedPtr
       turtle_coords_client_;
+
+  std::vector<std::vector<double>> coords_;
 };
 
 int main(int argc, char **argv) {
