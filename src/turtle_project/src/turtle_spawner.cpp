76f793043d80829ff5b836218b680145acf0b288
Arin Kumar <arinkumar10@outlook.com>
2026-04-08 18:12:10 +0000

    Created a node which spawns turtle and publishes a boolean value upon spawn.

---
+++ src/turtle_project/src/turtle_spawner.cpp
@@ -0,0 +1,95 @@
+#include "rclcpp/rclcpp.hpp"
+#include "turtle_project_interfaces/msg/spawned_turtle.hpp"
+#include "turtlesim/srv/spawn.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+#include <random>
+#include <string_view>
+#include <vector>
+
+class TurtleSpawnner : public rclcpp::Node {
+public:
+  TurtleSpawnner() : rclcpp::Node("turtle_spawnner") {
+    turtle_spawn_client_ = this->create_client<turtlesim::srv::Spawn>("/spawn");
+    spawned_turtle_info_publisher_ =
+        this->create_publisher<turtle_project_interfaces::msg::SpawnedTurtle>(
+            "turtle_spawn_info", 10);
+
+    this->timer_ = this->create_wall_timer(std::chrono::seconds(2),
+                                           [this]() { this->timerCallback(); });
+  }
+
+private:
+  void timerCallback() { this->spawnTurtle(); }
+
+  double getRandomNumber(double i = 1.0, double j = 10.0) {
+    std::random_device rd;
+    // 2. Initialize Mersenne Twister engine
+    std::mt19937 gen(rd());
+    // 3. Define the range [1.0, 10.0)
+    std::uniform_real_distribution<double> dis(i, j);
+
+    double randomNum = dis(gen);
+    return randomNum;
+  }
+
+  void spawnTurtle(std::string_view name = "") {
+
+    auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
+    request->x = getRandomNumber();
+    request->y = getRandomNumber();
+    request->theta = getRandomNumber();
+    request->name = name;
+
+    while (!this->turtle_spawn_client_->wait_for_service(
+        std::chrono::seconds(1))) {
+      RCLCPP_WARN(get_logger(), "Waiting for spawn server");
+    }
+
+    auto future = turtle_spawn_client_->async_send_request(
+        request,
+        [this,
+         request](rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture future) {
+          this->spawnFutureCallback(future, request);
+        });
+  }
+
+  void spawnFutureCallback(
+      rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture future,
+      turtlesim::srv::Spawn::Request::SharedPtr request) {
+
+    auto msg = turtle_project_interfaces::msg::SpawnedTurtle();
+    msg.spawned_turtle = true;
+    this->spawned_turtle_info_publisher_->publish(msg);
+
+    auto result = future.get();
+    currentX_ = request->x;
+    currentY_ = request->y;
+    currentYawInRads = request->theta;
+    RCLCPP_INFO(get_logger(), "Turtle Spawned at (%.2f, %.2f)\nName: %s",
+                currentX_, currentY_, result->name.c_str());
+
+    this->updateRecords();
+  }
+
+  void updateRecords() {
+    std::vector<double> coords{currentX_, currentX_, currentYawInRads};
+    this->coordinates.push_back(coords);
+  }
+
+  rclcpp::Publisher<turtle_project_interfaces::msg::SpawnedTurtle>::SharedPtr
+      spawned_turtle_info_publisher_;
+  rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr turtle_spawn_client_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  double currentX_, currentY_, currentYawInRads;
+  std::vector<std::vector<double>> coordinates;
+};
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<TurtleSpawnner>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
